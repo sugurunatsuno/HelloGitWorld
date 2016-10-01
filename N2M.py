c70b2137efd524ba349f65d14bdced41669c1d35
suguru irie <suguru.irie@gmail.com>
2016-10-01 07:48:55 -0700

    add class

--- N2M.py
+++ N2M.py
@@ -74,3 +74,46 @@ nth2ten("ff", 16)
 
 
 
+class BaseChenge(object):
+    def __init__(self):
+        self.decimal = 0
+        self.chenged = ""
+        self.base = 2
+        self.__character = "0123456789abcdefghijklmnopqrstuvwxyz"
+        self.__character_map = []
+        for i in self.__character:
+            self.__character_map.append(i)
+            
+    def set4base(self, base):
+        self.base = base
+    
+    def __basecheck(self):
+        if(self.base > 36):
+            self.base = 36
+        elif(self.base < 2):
+            self.base = 2
+            
+    def ten2nth(self, value):
+        self.__basecheck()
+        
+        output = []
+        while(value > 0):
+            output.append(value % self.base)
+            value = value // self.base
+        output.reverse()
+        output_character = ""
+        for i in output:
+            output_character = output_character + self.__character_map[i]
+        
+        self.chenged = output_character
+
+    def nth2ten(self, strvalue):
+        self.__basecheck()
+        
+        length = len(strvalue)
+        output = 0
+        for i in strvalue:
+            output = output + self.__character_map.index(i) * self.base ** (length - 1)
+            length = length - 1
+            
+        self.decimal = output
